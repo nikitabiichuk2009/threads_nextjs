16be45732b5373b9ed2bb33df7b773f7dbf9b783
Nikita Biichuk <niktestpython@gmail.com>
2024-07-01 14:34:50 +0300

    Some change

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,6 +8,8 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
+  // eslint-disable-next-line no-console
+  console.log(WEBHOOK_SECRET);
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
